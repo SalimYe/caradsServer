8de4eba33647763bad0994b11a3ec34a05911ae7
Florian Schäfffer <mail@florianschaeffer.de>
2016-04-17 23:16:24 +0200

    Add CarAds Client App

---
+++ src/main/webapp/app/commonFunctions.js
@@ -0,0 +1,34 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+function showModal($modal, modalDescription, modalTitle, modalButtonOneText, modalButtonTwoText, actionOne, actionTwo, angular) {
+
+    var opts = {
+        backdrop: 'static',
+        keyboard: false,
+        templateUrl: 'views/modalContent.html',
+        controller: 'ModalController',
+        resolve: {} // empty storage
+    };
+
+    opts.resolve.item = function () {
+        return angular.copy({description: modalDescription, title: modalTitle, buttonOne: modalButtonOneText, buttonTwo: modalButtonTwoText}); // pass data to Dialog
+    };
+
+    var modalInstance = $modal.open(opts);
+
+    modalInstance.result.then(function () {
+        //on close()
+        actionOne();
+    }, function () {
+        //on dismiss()
+        actionTwo();
+    });
+}
+;
---
+++ src/main/webapp/app/controllers/home.js
@@ -0,0 +1,4 @@
+app.controller('home', function ($scope, $translate) {
+
+    
+});
